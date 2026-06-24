3dbd062b2671b205b3e97f8ba7c28e3e27bf412e
Enwattao <enwattao@gmail.com>
2026-06-24 22:53:47 +0200

    Initial release ClimaPro v1.0.0

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_climapro_app_ClimaPro.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.climapro.app.ClimaPro"
-)
-public final class _com_climapro_app_ClimaPro {
-}
